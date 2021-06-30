edf31ddbf8f70acc9010d069b4958b57474aab62
raneemabujamous <>
2021-06-30 13:24:14 +0000

    apdute

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var gender= prompt("what is your gender? (male or female)");
+
+if(gender == 'male'){
+  alert("WE ARE SOORY THIS IS JUST FOR LADY PLEASE TELL YOR MOM,WIFE AND SISTER TO BE MORE SHIN ");
+}else if (gender == 'female'){
+  prompt("hi lady plese inter your wight ");
+  
+  
+}else{
+  alert("write correct spilling please ");
+}
+
